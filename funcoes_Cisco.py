20758e86f8d910da95575910e9a543dbe85c9291
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2019-07-02 20:22:03 -0300

    Add files via upload

---
+++ funcoes_Cisco.py
@@ -0,0 +1,683 @@
+﻿import requests
+import os
+import json 
+
+#########################################################
+## FUNCOES SMARTSHEET
+
+#########################################################
+
+def smartsheet(planilha):
+
+    # Este codigo abre uma determinada planilha no smartsheet e a retorna
+
+    #token esta na variável de ambiente
+    smartsheet_token=os.environ['SMART_TOKEN']
+
+    if "sec" in planilha:
+        sheet="1219329522984836"
+    elif "dna" in planilha:
+        sheet="835914437027716"
+    elif "collab" in planilha:
+        sheet="2994385685112708"
+    elif "dc" in planilha:
+        sheet="5992706112546692"
+    elif "sem" in planilha:
+        sheet="2210243842205572"
+    elif "pam" in planilha:
+        sheet="8872345856173956"
+
+
+    #planilha de managers
+    url = "https://api.smartsheet.com/2.0/sheets/"+sheet
+
+    payload = ""
+    headers = {
+        'Authorization': "Bearer "+ smartsheet_token,
+        'Content-Type': "application/json",
+        'cache-control': "no-cache",
+        'Postman-Token': "50a08645-32c6-4b43-9dcd-fb4db45c942e"
+        }
+
+    
+    response = requests.request("GET", url, data=payload, headers=headers)
+    
+    #pega conteudo pleno da planilha
+    if response.status_code==200:
+        json_res = json.loads(response.text)
+    else:
+    # devolve erro caso nao consiga acessar smartsheet
+        return "erro"
+
+    return json_res
+
+def smartse(parceiro,arquitetura,especialidade):
+
+    # Procura SE do parceiro, retorna msg com dados ou resultado negativo caso nao encontrado
+
+    if parceiro=="":
+        return
+
+    # planilha do smartsheet
+    # chama a funcao que busca planilha no smartsheet e devolve como JSON
+    data = smartsheet(arquitetura)
+
+    # aborta caso nao tenha sido possivel acessar smartsheet
+    if data=="erro":
+        msg="Erro de acesso\n"
+        return msg
+
+    # quantas linhas tem a planilha
+    linhas = data['totalRowCount']
+
+    #leva conteudo da planilha para variavel sheet
+    sheet=data['rows']
+
+    # loop para procurar o pam e imprime
+
+    msg=""
+    count=2
+    encontrado=0
+
+    while (count<linhas):
+
+        # valida 1 linha por vez
+        linha=data['rows'][count]
+
+        # acessa a primeira celula da linha (parceiro)
+        celulaparceiro=linha['cells'][0]['value']
+        
+        # gera a linha formatada caso parceiro encontrado
+        if parceiro in celulaparceiro.lower():
+            msg=msg+("  \n**Partner:**"+celulaparceiro+"  \n")
+            msg=msg+formata_SE(linha)
+            encontrado=encontrado+1
+        
+        count=count+1
+
+                    
+    # devolva negativa caso nada encontrado
+    if encontrado==0:
+        msg=msg+"SE: Nenhum resultado encontrado.  "
+
+
+
+    return msg
+
+def smartmanager(parceiro):
+
+    # Procura SEM do parceiro, retorna msg com dados ou resultado negativo caso nao encontrado
+
+    if parceiro=="":
+        return
+
+    # planilha do smartsheet
+    # chama a funcao que busca planilha no smartsheet e devolve como JSON
+    data = smartsheet("sem")
+
+
+    # aborta caso nao tenha sido possivel acessar smartsheet
+    if data=="erro":
+        msg="Erro de acesso\n"
+        return msg
+
+
+    # quantas linhas tem a planilha
+    linhas = data['totalRowCount']
+
+    #leva conteudo da planilha para variavel sheet
+    sheet=data['rows']
+
+    # loop para procurar o pam e imprime
+
+    msg=""
+    count=2
+    encontrado=0
+    
+    while (count<linhas):
+
+        # valida 1 linha por vez
+        linha=data['rows'][count]
+
+        # acessa a primeira celula da linha (parceiro)
+        celulaparceiro=linha['cells'][0]['value']
+        
+        # gera a linha formatada caso parceiro encontrado
+        if parceiro in celulaparceiro.lower():
+            msg=msg+formata_SEM(linha)
+            encontrado=encontrado+1
+        
+        count=count+1
+
+                    
+        # devolva negativa caso nada encontrado
+    
+    if encontrado==0:
+        msg="SEM: Nenhum resultado encontrado.  "
+
+
+    return msg
+
+def smartpam(parceiro):
+
+    # Procura PAM do parceiro, retorna msg com dados ou resultado negativo caso nao encontrado
+
+    if parceiro=="":
+        return
+
+    # planilha do smartsheet
+    # chama a funcao que busca planilha no smartsheet e devolve como JSON
+    data = smartsheet("pam")
+
+
+    # aborta caso nao tenha sido possivel acessar smartsheet
+    if data=="erro":
+        msg="Erro de acesso\n"
+        return msg
+
+    # quantas linhas tem a planilha
+    linhas = data['totalRowCount']
+
+    #leva conteudo da planilha para variavel sheet
+    sheet=data['rows']
+
+    # loop para procurar o pam e imprime
+
+    msg=""
+    count=2
+    encontrado=0
+    
+    while (count<linhas):
+
+        # valida 1 linha por vez
+        linha=data['rows'][count]
+
+        # acessa a primeira celula da linha (parceiro)
+        celulaparceiro=linha['cells'][0]['value']
+        
+        # gera a linha formatada caso parceiro encontrado
+        if parceiro in celulaparceiro.lower():
+            msg=msg+formata_PAM(linha)
+            encontrado=encontrado+1
+        
+        count=count+1
+
+                    
+    # devolva negativa caso nada encontrado
+    if encontrado==0:
+        msg="PAM: Nenhum resultado encontrado.  "
+
+
+    return msg
+
+
+
+def formata_SEM(dados):
+
+#monta linha do SEM
+
+# zera variaveis
+    
+    msg=""
+    semparceiro=""
+    semcity=""
+    semregion=""
+    semtitle=""
+    semname=""
+    semmail=""
+    semphone=""
+
+    # tenta pegar valores. Tenta pois se a celula estiver vazia, dará erro de conteúdo, por isto o 'try'
+    try:
+        semparceiro=dados['cells'][0]['value']
+    except:
+        pass
+    try:
+        semcity=dados['cells'][1]['value']
+    except:
+        pass
+    try:
+        semregion=dados['cells'][2]['value']
+    except:
+        pass
+    try:
+        semtitle=dados['cells'][3]['value']
+    except:
+        pass
+    try:
+        semname=dados['cells'][4]['value']
+    except:
+        pass
+    try:
+        semmail=dados['cells'][5]['value']
+    except:
+        pass
+    try:
+        semphone=dados['cells'][6]['value']
+    except:
+        pass
+
+    #monta a linha e imprime
+    
+    msg=msg+("**Manager:**"+semname+" **Partner:**"+semparceiro+" **Title:**"+semtitle+" "+semphone+" "+semmail+"  \n")
+    msg=msg+("**Region:**"+semregion+" "+semcity+"  \n\n")
+
+
+    return msg
+
+def formata_SE(dados):
+
+# monta linha do SE
+
+# zera variaveis
+    
+    msg=""
+    separceiro=""
+    sename=""
+    setel=""
+    semail=""
+    secomp=""
+    
+
+    # tenta pegar valores. Tenta pois se a celula estiver vazia, dará erro de conteúdo, por isto o 'try'
+    try:
+        separceiro=dados['cells'][0]['value']
+    except:
+        pass
+    try:
+        sename=dados['cells'][1]['value']
+    except:
+        pass
+    try:
+        setel=dados['cells'][2]['value']
+    except:
+        pass
+    try:
+        semail=dados['cells'][3]['value']
+    except:
+        pass
+    try:
+        secomp=dados['cells'][4]['value']
+    except:
+        pass
+    
+    #identifica competencias
+    compet=""
+    for x in secomp.split(','):
+        
+        # remove espacos em branco nas laterais
+        x=x.rstrip()
+        x=x.lstrip()
+            
+        if x !="":
+            compet = compet + " " + x + " "
+
+    #monta a linha e imprime
+   
+    msg=msg+("**SE:** "+sename+": "+semail+" "+setel+"  \n")
+    # imprime competencias somente se tiver
+    if compet != "":
+        msg=msg+("**Competencies:**"+compet+"  \n")
+            
+    return msg
+
+
+def formata_PAM(dados):
+
+#monta linha do PAM
+
+# zera variaveis
+    
+    msg=""
+    pamparceiro=""
+    pamname=""
+    pamcity=""
+    pammail=""
+    pamphone=""
+  
+    # tenta pegar valores. Tenta pois se a celula estiver vazia, dará erro de conteúdo, por isto o 'try'
+    try:
+        pamparceiro=dados['cells'][0]['value']
+    except:
+        pass
+    try:
+        pamcity=dados['cells'][1]['value']
+    except:
+        pass
+    try:
+        pamname=dados['cells'][2]['value']
+    except:
+        pass
+    try:
+        pammail=dados['cells'][3]['value']
+    except:
+        pass
+    try:
+        pamphone=dados['cells'][4]['value']
+    except:
+        pass
+    
+    #monta a linha e imprime
+    
+    msg=msg+("**PAM do Parceiro:** "+pamparceiro+": "+pamname+" "+pammail+"@cisco.com "+pamphone+" "+pamcity+"  \n\n")
+ 
+
+    return msg
+
+
+
+
+#########################################################
+## FUNCOES API Suporte Cisco
+
+#########################################################
+
+def help():
+
+    # Funcao ajuda deste bot
+    msg="""
+Forma de uso:  \n
+Procurar Manager do Parceiro: manager partner ***nome do parceiro*** OU  \n
+manager partner ***nome do manager***
+
+Procurar PAM do parceiro: pam partner ***nome do parceiro***
+
+Procurar SE do parceiro: se ***dna|dc|sec|collab*** partner ***nome do parceiro***
+
+Detalhe do Parceiro: detail partner ***nome do parceiro***
+"""
+    
+    return msg
+
+
+def getCiscoApiToken():
+    
+    #Chama Cisco.com para gerar token para consultas. Retorna Token
+
+    url = "https://cloudsso.cisco.com/as/token.oauth2"
+
+    # Dados criados de apiconsole.cisco.com
+
+    payload = "client_id=7t9s28h7t5337pwknwdesssv&grant_type=client_credentials&client_secret=7xtCggEKxGAYKs4UTHYaKUsD"
+    headers = {
+    'content-type': "application/x-www-form-urlencoded",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "f073e0db-6167-49c7-5e7a-54f4590331ff"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    resposta=json.loads(response.text)
+    token=resposta['access_token']
+
+    return token
+
+def CiscoApiHello (token):
+
+    # chama Cisco API para testes
+    url = "https://api.cisco.com/hello"
+
+    headers = {
+    'Authorization': "Bearer "+token,
+    'Cache-Control': "no-cache",
+    'Postman-Token': "9d6632a7-0d65-a22e-2274-3bac58526c7f"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.text
+
+def SupportAPIHello():
+
+    # Testa API Hello
+    token=getCiscoApiToken()
+    nova_resp=CiscoApiHello(token)
+    return nova_resp
+        
+    
+
+#########################################################
+## FUNCOES TECHMAPPING CISCO BRASIL
+
+#########################################################
+
+def procurase(parceiro,arquitetura,especialidade):
+
+    # Procura SE do parceiro, retorna msg com dados ou resultado negativo caso nao encontrado
+
+    # encerra se parceiro for nenhum (por enquanto)
+    if parceiro=="":
+        return
+
+    # O 3o parametro desta funcao e' para entender especialidades do SE
+    # caso tenha alguma, esta e' identificada, do contrario e' all (todas)
+    hbox=especialidade.split(arquitetura)
+    parametro=""
+    
+    if len(hbox)>1:
+        parametro=hbox[1]
+        especialidade=parametro.lstrip()
+        especialidade=especialidade.rstrip()
+    
+    if especialidade=="":
+        especialidade = "all"
+
+    msg = ""
+    count=0
+
+    # Base de dados de acordo com a arquitetura definida
+
+    if "sec" in arquitetura:
+        filepath = "BASE_SECURITY.txt"
+    if "dna" in arquitetura or "en" in arquitetura:
+        filepath = "BASE_EN.txt"
+    if "collab" in arquitetura:
+        filepath = "BASE_COLLAB.txt"
+    if "dc" in arquitetura or "data" in arquitetura:
+        filepath = "BASE_DC.txt"
+
+    # procura pessoa em parceiro especifico
+    # No futuro incluir pesquisa em todos os parceiros caso parceiro = all (todos)
+
+    if parceiro != "all":
+
+        # loop de pesquisa e criacao da resposta 
+        with open(filepath) as fp:  
+            line = fp.readline()
+            while line:
+                texto=line.split(";")
+                pname=texto[0]
+                    
+                # Caso encontrado o parceiro, investiga cada SE e competencias
+                if parceiro in pname.lower():
+                    if count == 0:
+                        msg=("**Partner:**"+pname+"  \n")
+
+                    sename=texto[1]
+                    setel=texto[2]
+                    semail=texto[3]
+                    secomp=texto[4]
+                                              
+                    #identifica competencias
+                    compet=""
+                    for x in secomp.split(','):
+                        if x !="":
+                            compet = compet + "'" + x + "'"
+
+                    # Seleciona se vai para impressao caso encontrado        
+
+                    # Se nenhuma competencia declarada, entao vale todas
+                    if especialidade == "all":
+                        
+                            msg=msg+("  \n**SE:** "+sename+": "+semail+" "+setel+"  \n")
+                            if compet != "":
+                                msg=msg+("**Competencies:**"+compet+"  \n")
+                            count=count+1
+                    
+                    # Se competencia declarada, entao somente aquele SE que a possui
+                    if especialidade != "all":
+                        if compet != "" and especialidade in compet.lower():
+                            msg=msg+("  \n**SE:** "+sename+": "+semail+" "+setel+"  \n")
+                            msg=msg+("**Competencies:**"+compet+"  \n")
+                            count=count+1
+
+                  
+
+                line = fp.readline()
+                        
+            # devolva negativa caso nada encontrado
+            if count==0:
+                msg="Nenhum resultado encontrado.  \n"
+
+        return msg     
+
+
+def procurapam(parceiro):
+
+    # Procura PAM do parceiro, retorna msg com dados ou resultado negativo caso nao encontrado
+
+    if parceiro=="":
+        return
+
+
+    msg = ""
+    count=0
+
+    # Procura o PAM do parceiro
+    # Base de dados
+    filepath = "basePAM.txt"
+
+    # loop de pesquisa  
+    with open(filepath) as fp:  
+        line = fp.readline()
+        while line:
+            texto=line.split(";")
+            pname=texto[0]
+                  
+            # Caso encontrado cria resposta
+            if parceiro in pname.lower():
+                pcity=texto[1]
+                ppam=texto[2]
+                pmail=texto[3]
+                pphone=texto[4]
+    
+                msg=msg+("**PAM do Parceiro:** "+pname+": "+ppam+" "+pmail+"@cisco.com "+pphone+" "+pcity+"  \n\n")
+                count=count+1
+                    
+            line = fp.readline()
+                    
+        # devolva negativa caso nada encontrado
+        if count==0:
+            msg="PAM: Nenhum resultado encontrado.  "
+
+    return msg     
+        
+def autorizauser(usermail):
+
+    # Esta funcao devolve true ou false para validar se usermail e' valido
+    # (se pode pedir comando ou nao)
+
+    # primeiro checa se email e da Cisco
+    email=usermail.split("@")
+        
+    # caso positivo devolve true ou false
+    if email[1]=="cisco.com":
+        resultado = True
+    else:
+        resultado = False
+   
+    return resultado
+
+def procuramanager(parceiro):
+
+    # Procura Detalhes do parceiro, retorna msg com dados ou resultado negativo caso nao encontrado
+
+    if parceiro=="":
+        return
+
+    msg=""
+    count = 0
+
+    
+    # Procura os Managers do parceiro
+    # Base de dados
+    filepath = "SEM.txt"
+
+    # loop de pesquisa  
+    with open(filepath) as fp:  
+        line = fp.readline()
+        while line:
+            texto=line.split(";")
+            pname=texto[0]
+                
+            # Caso encontrado cria resposta
+            if parceiro in pname.lower():
+                pcity=texto[1]
+                pregion=texto[2]
+                sem_title=texto[3]
+                sem_name=texto[4]
+                sem_mail=texto[5]
+                sem_phone=texto[6]
+                
+                msg=msg+("**Manager:**"+sem_name+" **Title:**"+sem_title+" "+sem_phone+" "+sem_mail+"  \n")
+                msg=msg+("**Region:**"+pregion+" **City:**"+pcity+"  \n\n")
+                count = count + 1
+                
+            line = fp.readline()
+
+    # tentativa de encontrar um SEM especifico caso a rotina acima nao retorne nada
+
+    if count==0:    
+
+        # loop de pesquisa  
+        with open(filepath) as fp:  
+            line = fp.readline()
+            while line:
+                texto=line.split(";")
+                manager=texto[4]
+                    
+                # Caso encontrado cria resposta
+                if parceiro in manager.lower():
+                    pname=texto[0]
+                    pcity=texto[1]
+                    pregion=texto[2]
+                    sem_title=texto[3]
+                    sem_name=texto[4]
+                    sem_mail=texto[5]
+                    sem_phone=texto[6]
+                    
+                    msg=msg+("**Manager:**"+sem_name+" **Partner:**"+pname+" **Title:**"+sem_title+" "+sem_phone+" "+sem_mail+"  \n")
+                    msg=msg+("**Region:**"+pregion+" "+pcity+"  \n\n")
+                    count = count + 1
+                    
+                line = fp.readline()
+
+
+
+        # devolva negativa caso nada encontrado
+        if count==0:
+            msg="Manager: Nenhum resultado encontrado.  \n"
+
+    return msg   
+
+def showtechmapping():
+
+    # retorna lista de nome de parceiros do Tech Mapping
+
+    msg = "\nLista de Parceiros Mapeados:\n"
+    
+    # Procura o PAM do parceiro
+    # Base de dados
+    filepath = "techmap.txt"
+
+    # loop de pesquisa  
+    with open(filepath) as fp:  
+        line = fp.readline()
+        while line:
+            texto=line.split(";")
+            pname=texto[0]
+                  
+            msg=msg+pname+"\n"
+
+            line = fp.readline()
+                    
+    return msg   
---
+++ logica.py
@@ -0,0 +1,100 @@
+﻿from funcoes_Cisco import *
+from funcoes_Mais import *
+
+def logica(comando,usermail):
+
+    # faz a logica de entender o comando pedido e a devida resposta para o usuario
+    # o parametro usermail e' utilizado para identificar o usuario que solicitou o comando
+    # O usuario pode ser uzado como filtro para se executar ou negar o comando
+    #
+    # Retorna mensagem para ser enviada para console ou Webex teams
+    
+    #Separa o comando por espacos
+    #Primeiro item e'o comando em si, os demais sao parametros deste comando
+    #
+    comando=comando.lower()
+    
+    # identifica e trata comandos relacionados a parceiros - palavra chave partner
+    # logo a primeira parte do comando e' o que queremos procurar e
+    # a segunda e' o nome do parceiro
+    # logo comando = o comando completo, box = funcao esperada e parceiro = nome do parceiro
+    sp=comando.split("partner")
+    # comando na variavel box
+    box=sp[0]
+
+    # ajusta variavel com o nome do parceiro eliminando espacos
+    if len(sp)>1:
+        parceiro=sp[1].lstrip()
+        # remove espacos no final, caso existam
+        parceiro=parceiro.rstrip()
+
+    
+    msg=""
+	
+    # chamadas de acordo com os parametros
+
+    # Funcoes somente para users Cisco
+
+    if autorizauser(usermail)==True:
+        # funcoes relacionadas a parceiro
+        if "partner" in comando:
+    
+            # chama os managers do parceiro
+            #if "manager" in box:
+            #    msg=procuramanager(parceiro)
+
+            if "manager" in box:
+                msg=smartmanager(parceiro)
+
+            # pam do parceiro
+            #if "pam" in box:
+            #    msg=procurapam(parceiro)
+            if "pam" in box:
+                msg=smartpam(parceiro)
+    
+            #if "se" in box or "systems engineer" in box:
+            #    if "sec" in box:
+            #        msg=procurase(parceiro,"sec",box)
+            #    elif "dc" in box:
+            #        msg=procurase(parceiro,"dc",box)
+            #    elif "dna" in box:
+            #        msg=procurase(parceiro,"dna",box)
+            #    elif "col" in box:
+            #        msg=procurase(parceiro,"collab",box)
+            #    else:
+            #        msg="use: se ***dc|dna|sec|collab*** partner ***partner name***"
+
+            if "se" in box:
+                if "sec" in box:
+                    msg=smartse(parceiro,"sec",box)
+                elif "dc" in box:
+                    msg=smartse(parceiro,"dc",box)
+                elif "dna" in box:
+                    msg=smartse(parceiro,"dna",box)
+                elif "col" in box:
+                    msg=smartse(parceiro,"collab",box)
+                else:
+                    msg="use: se ***dc|dna|sec|collab*** partner ***partner name***"
+
+
+            # detalhe completo do parceiro
+            #if "detail" in box and parceiro != "":
+            #    msg=procurapam(parceiro)
+            #    msg=msg+procuramanager(parceiro)
+
+            if "detail" in box and parceiro != "":
+                msg=smartpam(parceiro)
+                msg=msg+smartmanager(parceiro)
+
+    
+    if "help" in comando:
+        msg=help()
+
+    # Funcoes para todos
+
+    
+
+    if msg=="" or msg==None:
+        msg="Use 'help' for help :-)"
+
+    return msg
